722b9a452f19f71230bcb4945309e90e94e45c04
Klever-Sobrinho <kleverfortaleza@gmail.com>
2019-02-18 10:01:57 -0300

    using other examples with subject

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -18,3 +18,28 @@ describe Calculator do
     end
   end
 end
+=begin
+-----Example 2 (Subject)
+describe Calculator do
+
+  subject(:calc) { described_class.new }
+
+  context '#sum' do
+    it 'with positive numbers' do
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
+end
+
+------Example 3 (Subject)
+describe 'Classe Calculadora' do
+
+  subject(:calc) { Calculator.new }
+
+  context '#sum' do
+    it 'with positive numbers' do
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)
+    end
+end
+=end
