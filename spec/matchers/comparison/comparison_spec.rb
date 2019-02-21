85ddfd70c674c53fbb59a411df0f94d2e15b4417
Klever-Sobrinho <kleverfortaleza@gmail.com>
2019-02-21 10:42:29 -0300

    aggregate failures

--- spec/matchers/comparison/comparison_spec.rb
+++ spec/matchers/comparison/comparison_spec.rb
@@ -23,6 +23,18 @@ describe 'Matchers Comparison' do
     expect(7).to be_between(2,7).inclusive 
   end
 
+  # it 'be_between inclusive / Aggregate failures', :aggregate_failures do
+  #   expect(5).to be_between(2,7).inclusive
+  #   expect(1).to be_between(2,7).inclusive 
+  #   expect(8).to be_between(2,7).inclusive 
+  # end
+
+  it 'be_between inclusive / Aggregate failures' do
+    expect(5).to be_between(2,7).inclusive
+    expect(1).to be_between(2,7).inclusive 
+    expect(8).to be_between(2,7).inclusive 
+  end
+
   it 'be_between exclusive' do
     expect(5).to be_between(2,7).exclusive
     expect(3).to be_between(2,7).exclusive 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,10 @@ require_relative '../app/helpers/helper'
 
 RSpec.configure do |config|
 
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true
+  end
+
   # config.before(:suite) do
   #   puts ">>>>>>>> Before test suite"
   # end
